63d8ce790b480c80a0258dcf18735eee645e050e
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2025-12-09 20:42:26 -0300

    frontend: agregar soporte de internacionalización (i18n)
    - Configurar react-i18next
    - Agregar traducciones en español e inglés
    - Soporte para múltiples idiomas en la aplicación

---
+++ homework2/application_development/frontend/src/i18n/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import esTranslations from './locales/es.json'
+import enTranslations from './locales/en.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      es: {
+        translation: esTranslations
+      },
+      en: {
+        translation: enTranslations
+      }
+    },
+    fallbackLng: 'es',
+    supportedLngs: ['es', 'en'],
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage']
+    },
+    interpolation: {
+      escapeValue: false
+    },
+    react: {
+      useSuspense: false
+    }
+  })
+
+export default i18n
+
+
