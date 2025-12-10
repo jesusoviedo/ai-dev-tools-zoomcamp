7356c612ba0b61a95d2e277c456f8a8eefe7588e
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2025-12-09 21:36:38 -0300

    backend: corregir warning de declarative_base usando sqlalchemy.orm

--- homework2/application_development/backend/app/database.py
+++ homework2/application_development/backend/app/database.py
@@ -2,8 +2,7 @@
 
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from typing import Generator
 
 # Get database URL from environment variable
